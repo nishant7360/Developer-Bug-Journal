d35a9f727637980f79930fc2151c4626db76773d
Nishant Yadav <nishanty736@gmail.com>
2026-08-24 10:52:50 +0530

    add getUserQuestions, getUserAnswers

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -6,9 +6,7 @@ import ApiResponse from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
 
 export const getUserProfile = asyncHandler(async (req, res) => {
-  const { username } = req.params;
-
-  const user = await User.findOne({ username }).select(
+  const user = await User.findById(req.user._id).select(
     "username profileImage createdAt",
   );
 
@@ -55,4 +53,82 @@ export const getUserProfile = asyncHandler(async (req, res) => {
 
 export const getUserQuestions = asyncHandler(async (req, res) => {
   const { page = 1, limit = 10 } = req.query;
+
+  const pageNunber = Number(page);
+  const limitNumber = Number(limit);
+
+  if (!Number.isInteger(pageNunber) || pageNunber < 1) {
+    throw new ApiError(400, "Page must be a positive integer");
+  }
+
+  if (!Number.isInteger(limitNumber) || limitNumber < 1 || limitNumber > 50) {
+    throw new ApiError(400, "Limit must be between 1 and 50");
+  }
+
+  const skip = (pageNunber - 1) * limitNumber;
+
+  const totalQuestions = await Question.countDocuments({
+    author: req.user._id,
+  });
+
+  const questions = await Question.find({
+    author: req.user._id,
+  })
+    .populate("tags", "name description")
+    .sort({ createdAt: -1 })
+    .skip(skip)
+    .limit(limitNumber);
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        result: questions.length,
+        totalQuestions,
+        currentPage: pageNunber,
+        totalPages: Math.ceil(totalQuestions / limitNumber),
+        questions,
+      },
+      "Your questions fetched successfully",
+    ),
+  );
+});
+
+export const getUserAnswers = asyncHandler(async (req, res) => {
+  const { page = 1, limit = 10 } = req.query;
+
+  const pageNumber = Number(page);
+  const limitNumber = Number(limit);
+  
+  if (!Number.isInteger(pageNumber) || page < 1) {
+    throw new ApiError(400, "Page number must be integer");
+  }
+
+  if (!Number.isInteger(limitNumber) || limit < 1 || limit > 50) {
+    throw new ApiError(400, "Limit must be between 1 and 50");
+  }
+
+  const skip = (pageNumber - 1) * limitNumber;
+
+  const totalAnswers = await Answer.countDocuments({ author: req.user._id });
+
+  const answers = await Answer.find({ author: req.user._id })
+    .populate("question")
+    .sort({ createdAt: -1 })
+    .skip(skip)
+    .limit(limitNumber);
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        result: answers.length,
+        totalAnswers,
+        currentPage: pageNumber,
+        totalPages: Math.ceil(totalAnswers / limitNumber),
+        answers,
+      },
+      "Your answers fetched successfully",
+    ),
+  );
 });
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,10 +1,17 @@
 import express from "express";
 
 import { protect } from "../middlewares/auth.middleware.js";
-import { getUserProfile } from "../controllers/user.controller.js";
+import {
+  getUserAnswers,
+  getUserProfile,
+  getUserQuestions,
+} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.get("/:username", protect, getUserProfile);
+router.get("/", protect, getUserProfile);
+
+router.get("/my-questions", protect, getUserQuestions);
+router.get("/my-answers", protect, getUserAnswers);
 
 export default router;
